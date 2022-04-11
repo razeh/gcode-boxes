a502609ec59e7eefc99fecd515b6efe6b68fd62a
Robert Allan Zeh <robert.a.zeh@gmail.com>
2022-04-10 21:05:40 -0500

    Looks good in the viewer

--- bit_info.py
+++ bit_info.py
@@ -1,15 +1,17 @@
 import math
 
 class BitInfo(object):
-    def __init__(self, largest_bit_diameter, bit_angle):
+    def __init__(self, largest_bit_diameter, bit_angle, depth_of_cut):
         " Bit angle is in radians. "
         self.largest_bit_diameter = largest_bit_diameter
         self.bit_angle = bit_angle
+        self.depth_of_cut = depth_of_cut
     
     def diameter_at_height(self, height):
         return self.largest_bit_diameter - (height * math.tan(self.bit_angle))
 
-    
-DOVETAIL_BIT_1 = BitInfo(25.4/2.0, 14 * (math.pi/180.0))
-DOVETAIL_BIT_2 = BitInfo((5.0/16.0) * 25.4, 8 * (math.pi/180.0))
-STRAIGHT_BIT_1 = BitInfo(24.4/4, 0.0)
+TO_MM=25.4  # number of milimeters in an inch
+DOVETAIL_BIT_1 = BitInfo(TO_MM/2.0, 14 * (math.pi/180.0), TO_MM/2.0)
+DOVETAIL_BIT_2 = BitInfo((5.0/16.0) * TO_MM, 8 * (math.pi/180.0), TO_MM/2.0)
+DOVETAIL_BIT_3 = BitInfo(TO_MM*.75, 14 * (math.pi/180.0), (14.0/16.0)*TO_MM)
+STRAIGHT_BIT_1 = BitInfo(24.4/4, 0.0, TO_MM)
--- boat_box.py
+++ boat_box.py
@@ -4,7 +4,10 @@ from __future__ import print_function
 from mecode import GMatrix
 from bit_info import DOVETAIL_BIT_1
 from bit_info import DOVETAIL_BIT_2
+import math
 
+def extra_width_for_depth(z, bit):
+    return -math.tan(bit.bit_angle) * z
 
 class boatbox(object):
     def __init__(self,
@@ -24,6 +27,7 @@ class boatbox(object):
         print('; y_length =', self.y_length)
         print('; z_length =', self.z_length)
         print('; z_length_increment =', self.z_length_increment)
+        print('; bit_diameter=', self.bit_diameter)
 
     @property
     def bit_diameter(self):
@@ -33,45 +37,67 @@ class boatbox(object):
     def bit_radius(self):
         return self.bit_diameter/2.0
 
+    def x_length_at_z(self, z):
+        return self.x_length - self.bit_radius + 2*extra_width_for_depth(z, self.bit_info)
+
+    def y_length_at_z(self, z):
+        return self.y_length - self.bit_radius + 2*extra_width_for_depth(z, self.bit_info)
+
+    def offset_for_z(self, z):
+        return extra_width_for_depth(z, self.bit_info)
+
     def clear_box(self):
-        x_length = self.x_length - self.bit_radius
-        y_length = self.y_length - self.bit_radius
         g = self.g
         z = 0
+        def spiral_box():
+            self.spiral_box(self.x_length_at_z(z), self.y_length_at_z(z)) 
 
-        def box():
-            g.abs_move(x=0, y=0)
-            g.meander(x_length, y_length, self.bit_diameter, 'LL', 'y')
-
-        while z > -self.z_length:
-            box()
-            z -= self.z_length_increment
-            g.move(z=self.z_length_increment)
-            g.abs_move(z=z)
+        def move_to_start_position():
+            g.abs_move(x=-self.offset_for_z(z)+self.bit_radius*2.0,
+                       y=-self.offset_for_z(z))
+            g.abs_move(x=-self.offset_for_z(z), y=-self.offset_for_z(z))
             
-        if (z != -self.z_length):
+        while True:
+            print('; starting spiral box z=', z)
+            spiral_box()
+            print('; finished spiral box z=', z)
+            last_z = z
+            z -= self.z_length_increment
+            if z < -self.z_length:
+                break
+            g.abs_move(z=z)
+            move_to_start_position()
+            
+        if (last_z != -self.z_length):
             g.abs_move(z=-self.z_length)
-            box()
+            move_to_start_position()
+            spiral_box()
 
-    def bottom_wedge(self):
-        g = self.g        
-        g.abs_move(x=0, y=0)
-        wedge_depth = self.bit_info.largest_bit_diameter - self.bit_info.smallest_bit_diameter
-        g.move(y=-wedge_depth)
-        g.move(x=self.x_length)
-        g.move(x=-self.bit_diameter-1)
-        g.abs_move(y=self.y_length+wedge_depth, x=self.x_length)
-        g.move(x=-self.x_length)
-        g.abs_move(x=self.x_length/2.0, y=self.y_length/2.0)
-        g.abs_move(z=0.0)
+    def spiral_box(self, x_length, y_length):
+        print('; spiral box ', x_length, y_length)
+        g = self.g
+        bit_radius = self.bit_diameter/2.0
+        y_delta = y_length - bit_radius
+        x_delta = x_length - bit_radius
+        if y_delta < 0: return
+        if x_delta < 0: return
+        overlap = 1.0
+        g.move(y=y_delta)
+        g.move(x=x_delta)
+        g.move(y=-y_delta)
+        g.move(x=-x_delta+self.bit_diameter)
+        g.move(y=self.bit_diameter)
+        self.spiral_box(x_length-self.bit_diameter*2*overlap,
+                        y_length-self.bit_diameter*2*overlap)
 
     def box(self):
         self.g.feed(250)
         self.clear_box()
-        self.bottom_wedge()
         
 
 if __name__ == "__main__":
-    bb = boatbox(45.0, 20.00, 25.0, 1, DOVETAIL_BIT_2)
+    bit = DOVETAIL_BIT_2
+    bb = boatbox(50.0, 60.0, bit.depth_of_cut, 1, bit)
     bb.box()
+    #bb.spiral_box(80, 100)
     
